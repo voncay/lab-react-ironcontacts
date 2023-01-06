1b8658e91a1bbec1aa14f8645ebead5f88a7f378
Barbara <barbara@git>
2023-01-06 18:13:40 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
+import { useState } from 'react'
 
 function App() {
+  let myContacts = contacts.slice(0,5);
+  const [contactsData, setContactsData] = useState(myContacts);
+  const remainingContacts = contacts.slice(5, contacts.length)
+
+  const addRandomContact = (array) => {
+    let randomCeleb = {}
+    randomCeleb = array[ Math.floor(Math.random() * array.length) ];
+    const exists = contactsData.some( e => e.id === randomCeleb.id)
+    console.log(exists)
+    if (!exists) {
+      setContactsData(contactsData => [...contactsData, randomCeleb])
+    }
+    console.log(contactsData)
+  }
+
+  const removeContact = (contactId) => {
+    setContactsData(current =>
+      current.filter(e => {
+        return e.id !== contactId;
+      }),
+    );
+  }
+
+  const sortContactByName = (array) => {
+    const sorted = array.sort((a, b) => {
+      let fa = a.name.toLowerCase(),
+          fb = b.name.toLowerCase();
+      if (fa < fb) {  return -1 }
+      if (fa > fb) { return 1 }
+      return 0;
+    });
+    setContactsData(contactsData => [...sorted]);
+  }
+
+  const sortContactByPopularity = (array) => {
+    const sorted = array.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    setContactsData(contactsData => [...sorted]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <div className="buttonsContainer">
+        <button onClick={ () => addRandomContact(remainingContacts)}>Add Random Contact</button>
+        <button onClick={ () => sortContactByName(contactsData)}>Sort contact by name</button>
+        <button onClick={ () => sortContactByPopularity(contactsData)}>Sort contact by popularity</button>
+      </div>
+      <table style={{width:"100%"}}>
+        <thead>
+          <tr>
+            <th style={{width:"30%"}}>>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contactsData.map( e => {
+            return (
+              <tr key={e.id}>
+                <td><img src={e.pictureUrl} alt=""  width="100%" height="100%"/></td>
+                <td>{e.name}</td>
+                <td>{e.popularity.toFixed(2)}</td>
+                <td >{e.wonOscar && '🏆'}</td>
+                <td>{e.wonEmmy && '🏆'}</td>
+                <td><button onClick={ () => removeContact(e.id)}>Delete</button></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
